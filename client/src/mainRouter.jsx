6ad93ac6e22f7278a908a9efe4de6616cab3a4ae
JViniX <47263194+JViniX@users.noreply.github.com>
2026-07-08 20:37:57 -0400

    Week08

--- client/src/mainRouter.jsx
+++ client/src/mainRouter.jsx
@@ -9,6 +9,7 @@ import References from "./components/References";
 import Contact from "./components/Contact";
 import NotFound from "./components/NotFound";
 import Layout from "./components/Layout";
+import ListProjet from "./components/project/ListProjects";
 
 function MainRouter(){
     return(
@@ -17,10 +18,11 @@ function MainRouter(){
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/about" element={<About />} />
-                <Route exact path="/projects" element={<Projects />} />
+                {/* <Route exact path="/projects" element={<Projects />} /> */}
                 <Route exact path="/services" element={<Services />} />
                 <Route exact path="/references" element={<References />} />
                 <Route exact path="/contact" element={<Contact />} />
+                <Route exact path="/projects" element={<ListProjet />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </div>
